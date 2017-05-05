b6c9a9cbed79266f011cb06444334acd0e53c996
Leiner <leiner_0304@hotmail.com>
2017-05-05 14:03:44 -0500

    Principal Terminado

---
+++ app/src/main/java/com/example/android/listviewpersonalizado/Principal.java
@@ -0,0 +1,48 @@
+package com.example.android.listviewpersonalizado;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.w3c.dom.ls.LSInput;
+
+public class Principal extends AppCompatActivity {
+    private ListView ls;
+    private Resources res;
+    private String[] opc;
+    private Intent i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        ls = (ListView)findViewById(R.id.lvopciones);
+        res = this.getResources();
+        opc = res.getStringArray(R.array.opciones);
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,opc);
+        ls.setAdapter(adapter);
+        ls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        i = new Intent(Principal.this, Principal.class);
+                        startActivity(i);
+                        break;
+                    case 1:
+                        i = new Intent(Principal.this, Principal.class);
+                        startActivity(i);
+                        break;
+                    case 2:
+                        i = new Intent(Principal.this, Principal.class);
+                        startActivity(i);
+                        break;
+                }
+            }
+        });
+    }
+}
